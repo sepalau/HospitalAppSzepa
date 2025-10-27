43109de9bcb8235b45567496e3e8b67979ef5c3f
Krysss <BanerBrake@gmail.com>
2025-10-27 14:23:24 +0200

    Appointment repo

---
+++ demo/src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+import com.example.demo.model.Appointment;
+import com.example.demo.model.Department;
+
+import java.util.List;
+
+public class AppointmentRepository {
+    public void create(Appointment appointments) {
+    }
+
+    public List<Appointment> readAll() {
+        return null;
+    }
+
+    public void update(String id, Department updatedDepartment) {
+    }
+
+    public void delete(String id) {
+    }
+
+}
