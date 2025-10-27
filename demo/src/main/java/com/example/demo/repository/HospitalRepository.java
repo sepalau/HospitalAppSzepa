43fcad904a0a2aeb96fc4d567a839868b8277bed
Krysss <BanerBrake@gmail.com>
2025-10-27 14:03:01 +0200

    Repo hospital

---
+++ demo/src/main/java/com/example/demo/repository/HospitalRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+import com.example.demo.model.Hospital;
+import java.util.List;
+
+public class HospitalRepository {
+    public void create(Hospital hospital) {
+    }
+
+    public List<Hospital> readAll() {
+        return null;
+    }
+
+    public void update(String id, Hospital updatedHospital) {
+    }
+
+    public void delete(String id) {
+    }
+}
