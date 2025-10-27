d998e8afc4557458401013942c2b897731e19abe
Serban <serban.trisca@stud.ubbcluj.ro>
2025-10-27 14:11:58 +0200

    doctor and nurse clases

---
+++ demo/src/main/java/com/example/demo/model/Doctor.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Doctor extends MedicalStaff {
+    private String licenseNumber;
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Nurse.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class Nurse extends MedicalStaff {
+    private String qualificationLevel;
+
+    public String getQualificationLevel() {
+        return qualificationLevel;
+    }
+    public void setQualificationLevel(String qualificationLevel) {
+        this.qualificationLevel = qualificationLevel;
+    }
+}
